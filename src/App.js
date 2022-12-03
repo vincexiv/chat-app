f058b0aae2df55c4fbce5028dc9aec1d7cab7a48
Vincent Makokha <makokhavomondi@gmail.com>
2022-12-03 11:29:15 +0300

    Add background

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import Background from "./components/Background";
 
 function App(){
   return (
-    <Navbar loggedIn={false}/>
+    <>
+      <Navbar loggedIn={false}/>
+      <Background />
+    </>
   )
 }
 
---
+++ src/components/Background.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "../css/background.css"
+
+function Background(){
+    return (
+        <div className="background"></div>
+    )
+}
+
+export default Background
