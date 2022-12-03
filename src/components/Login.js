1e664f5bab5d7e559c42b4aca8c2a12afddd84d5
Vincent Makokha <makokhavomondi@gmail.com>
2022-12-03 17:17:37 +0300

    Add Login and Signup pages

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,28 @@
 import React from "react";
+import "../css/login-form.css"
 
-function Login(){
+function Login() {
     return (
-        <>
-            <div></div>
-        </>
+        <div className="login">
+            <div className="container">
+                <form>
+                    <div className="input">
+                        <label for="username">Username</label>
+                        <input type="text" name="username" placeholder="johndoe" required />
+                    </div>
+
+                    <div className="input">
+                        <label for="profile_picture">Password</label>
+                        <input type="password" />
+                    </div>
+
+                    <div className="buttons">
+                        <button className="btn">Signup</button>
+                        <button className="btn">Login</button>
+                    </div>
+                </form>
+            </div>
+        </div>
     )
 }
 
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,8 +1,48 @@
 import React from "react";
+import "../css/login-form.css"
 
 function Signup(){
     return (
-        <div></div>
+        <div className="signup">
+            <div className="container">
+                <form>
+                    <div className="input">
+                        <label for="full_name">Full Name</label>
+                        <input type="text" name="full_name" placeholder="John Doe" required />
+                    </div>
+
+                    <div className="input">
+                        <label for="email">Email</label>
+                        <input type="email" name="email" placeholder="johndoe@example.com" required />
+                    </div>
+
+                    <div className="input">
+                        <label for="username">Username</label>
+                        <input type="text" name="username" placeholder="johndoe" required />
+                    </div>
+
+                    <div className="input">
+                        <label for="username">Password</label>
+                        <input type="password" name="username" required />
+                    </div>
+
+                    <div className="input">
+                        <label for="bio">Bio</label>
+                        <textarea name="bio" placeholder="Here for a good time" />
+                    </div>
+
+                    <div className="input">
+                        <label for="profile_picture">Profile Picture</label>
+                        <input type="text" name="profile_picture" />
+                    </div>
+
+                    <div className="buttons">
+                        <button className="btn">Signup</button>
+                        <button className="btn">Login</button>
+                    </div>
+                </form>
+            </div>
+        </div>
     )
 }
 
