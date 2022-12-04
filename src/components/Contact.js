329824112c44da9210bb3be7ccb3f318961f83d3
Vincent Makokha <makokhavomondi@gmail.com>
2022-12-04 13:20:19 +0300

    Show contacts

---
+++ src/components/Contact.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "../css/contact.css"
+
+function Contact({contact}){
+    console.log(contact)
+    return (
+        <div className="contact">
+            <div className="status"></div>
+            <div className="full-name">
+                <h1>{contact.fullName}</h1>
+            </div>
+            <div className="views">
+                <div className="view-count">
+                    <p>20</p>
+                </div>
+            </div>         
+        </div>
+    )
+}
+
+export default Contact
