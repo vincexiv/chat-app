3bd7ef728140a4da65b24df82cdbeec4264c1faa
Vincent <makokhavomondi@gmail.com>
2023-04-18 23:48:10 +0300

    Show the number of unreplied messages

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,13 +3,42 @@ import { userDetails } from "./UserDetailsContextProvider";
 import "../css/contact.css"
 
 function Contact({ contact, onChatWith, toggleItemToShow }){
-    const {messages, they} = useContext(userDetails)
+    const {messages, me, they} = useContext(userDetails)
 
     function handleClick(){
         toggleItemToShow()
         onChatWith(contact.userId)
     }
-    
+
+    function compareTime(a, b){
+        const date1 = new Date(a.created_at)
+        const date2 = new Date(b.created_at)
+
+        return date1.getTime() - date2.getTime()
+    }
+
+    function getMyMessages(){
+        return messages.filter(message => {
+            return (message.sender === me.id) && (message.receiver === contact.userId)
+        })
+    }
+
+    function unrepliedMessages(){
+        const myMessages = getMyMessages()
+
+        const myMostRecentMessage = myMessages.sort(compareTime)[myMessages.length - 1]  
+              
+        const theirUnrepliedMessages = messages.filter(message => {
+            if(myMessages.length){
+                return message.sender === contact.userId && message.created_at > myMostRecentMessage?.created_at
+            }else{
+                return message.sender === contact.userId
+            }            
+        })
+
+        return theirUnrepliedMessages.length
+    }
+
     return (
         <div className="contact" onClick={(e)=> handleClick() }>
             <div className="status"></div>
@@ -18,16 +47,7 @@ function Contact({ contact, onChatWith, toggleItemToShow }){
             </div>
             <div className="views">
                 <div className="view-count">
-                    <p>
-                        
-                        {
-                            
-                            messages && they ? 
-                            messages.filter(message => message.sender === contact.userId).length :
-                            ""
-                        }
-                    </p>
-                
+                    <p> {unrepliedMessages()}</p>
                 </div>
             </div>         
         </div>
